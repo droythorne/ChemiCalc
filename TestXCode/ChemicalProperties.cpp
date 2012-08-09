4f418a8a5b1cdcca57fc4f777de9d987a5d8d09d
Daniel Roythorne <danielroythorne@informa-jral7wq.home>
2012-08-09 10:51:06 +0100

    Deleted TestXCode directory

--- TestXCode/ChemicalProperties.cpp
+++
@@ -1,9 +0,0 @@
-//
-//  ChemicalProperties.cpp
-//  ChemiCalc
-//
-//  Created by Daniel Roythorne on 29/07/2012.
-//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
-//
-
-#include <iostream>
--- TestXCode/InputParser.cpp
+++
@@ -1,9 +0,0 @@
-//
-//  InputParser.cpp
-//  ChemiCalc
-//
-//  Created by Daniel Roythorne on 30/07/2012.
-//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
-//
-
-#include <iostream>
